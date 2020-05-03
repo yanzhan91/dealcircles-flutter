3acc2e898f8287a6935b24b7e933eebc5330eb82
claireyang7725 <claireyang7725@gmail.com>
2020-05-03 00:06:51 -0500

    Show no result screen

--- lib/deals_view.dart
+++ lib/deals_view.dart
@@ -219,7 +219,42 @@ class _DealsViewState extends State<DealsView> {
         ),
       );
     } else {
-      return null;
+      return Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 80),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.remove_circle_outline,
+                size: 60,
+                color: Theme.of(context).primaryColor,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "No Results Found",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("Check out popular deals"),
+                ),
+                textColor: Colors.white,
+                onPressed: () {
+                  setFilters();
+                  loadDeals();
+                },
+              ),
+            ],
+          ),
+        )
+      );
     }
   }
 
@@ -314,13 +349,13 @@ class _DealsViewState extends State<DealsView> {
         await ApiService.loadDeals(sort, category, search, deals.length);
 
     if (newDeals.length == 0) {
-      ableToLoadMore = false;
       setState(() {
+        ableToLoadMore = false;
         loading = false;
       });
     } else {
-      ableToLoadMore = true;
       setState(() {
+        ableToLoadMore = newDeals.length == 30;
         deals.addAll(newDeals);
         loading = false;
       });
