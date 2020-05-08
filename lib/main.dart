3737fd7a26b785a9fdb3ff5928d472900da2dfb0
claireyang7725 <claireyang7725@gmail.com>
2020-05-07 22:44:54 -0500

    Add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dealcircles_flutter/theme_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'deals_view.dart';
 
@@ -19,6 +20,7 @@ class DealsCircleApp extends StatelessWidget {
       title: 'DealCircles',
       theme: ThemeData(
         primarySwatch: ThemeColors.primary_color,
+        fontFamily: GoogleFonts.openSans().fontFamily
       ),
       home: home,
       debugShowCheckedModeBanner: false
