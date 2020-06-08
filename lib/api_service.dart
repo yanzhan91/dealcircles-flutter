540d445f6abd026bfc2845d97a6a3410f84268d2
claireyang7725 <claireyang7725@gmail.com>
2020-06-08 03:01:26 -0500

    Add click events

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,19 +1,34 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:device_info/device_info.dart';
 import 'package:http/http.dart' as http;
 import 'Deal.dart';
 
 class ApiService {
   static const String BASE_URL = "https://api.dealcircles.com";
+  static String deviceId;
+  static String deviceName;
 
   static void addClicks(Deal deal) async {
-    String url = "$BASE_URL/clicks?id=${deal.id}";
+    Map<String, String> deviceInfo = await _getDeviceId();
+    String url = "$BASE_URL/clicks";
     print(url);
-    http.post(url);
+    http.post(
+      url,
+      body: jsonEncode(
+          <String, String>{
+            'id': deal.id,
+            'deviceId': deviceInfo['deviceId'],
+            'deviceName': deviceInfo['deviceName']
+          }),
+    );
   }
 
-  static Future<List> loadDeals(
-      String sort, String category, String search, int length) async {
-    String url = "$BASE_URL/deals?offset=$length";
+  static Future<List> loadDeals(String sort, String category, String search,
+      int length) async {
+    Map<String, String> deviceInfo = await _getDeviceId();
+    String url = "$BASE_URL/deals?"
+        "deviceId=${deviceInfo['deviceId']}&deviceName=${deviceInfo['deviceName']}&offset=$length";
 
     List queryParam = [];
     if (sort != null) {
@@ -54,5 +69,31 @@ class ApiService {
       print('Failed to load deals ${response.statusCode}: ${response.body}');
       return [];
     }
+  }
+
+  static Future<Map<String, String>> _getDeviceId() async {
+    if (deviceId == null || deviceName == null) {
+      try {
+        DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+        if (Platform.isAndroid) {
+          AndroidDeviceInfo androidDeviceInfo =
+          await deviceInfoPlugin.androidInfo;
+          deviceId = androidDeviceInfo.androidId;
+          deviceName = androidDeviceInfo.device;
+        } else if (Platform.isIOS) {
+          IosDeviceInfo iosDeviceInfo = await deviceInfoPlugin.iosInfo;
+          deviceId = iosDeviceInfo.identifierForVendor;
+          deviceName = iosDeviceInfo.name;
+        }
+      } on Exception {
+        deviceId = '';
+        deviceName = '';
+      }
+    }
+
+    return {
+      'deviceId': deviceId,
+      'deviceName': deviceName,
+    };
   }
 }
