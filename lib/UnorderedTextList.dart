5515601a9ff310589e42d5857a663a2a113e9dda
claireyang7725 <claireyang7725@gmail.com>
2020-07-22 01:00:53 -0500

    Add deal descriptions

---
+++ lib/UnorderedTextList.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class UnorderedTextList extends StatelessWidget {
+  UnorderedTextList(this.texts, this.textStyle);
+  final List<String> texts;
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    var widgetList = <Widget>[];
+    for (var text in texts) {
+      // Add list item
+      widgetList.add(UnorderedTextListItem(text, textStyle));
+      // Add space between items
+      widgetList.add(SizedBox(height: 5.0));
+    }
+
+    return Column(children: widgetList);
+  }
+}
+
+class UnorderedTextListItem extends StatelessWidget {
+  UnorderedTextListItem(this.text, this.textStyle);
+  final String text;
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("\u2022 ", style: textStyle),
+        Expanded(
+          child: Text(text, style: textStyle,),
+        ),
+      ],
+    );
+  }
+}
