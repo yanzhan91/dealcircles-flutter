62a76a334baa467129d64a371669bec4cc52345e
claireyang7725 <claireyang7725@gmail.com>
2020-08-05 22:26:19 -0500

    Expand theme icons gesture area

--- lib/deals_view.dart
+++ lib/deals_view.dart
@@ -368,141 +368,147 @@ class _DealsViewState extends State<DealsView> {
     return Container(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
-        child: Row(
+        child: Stack(
           children: <Widget>[
-            Image.network(
-              deal.images.length > 0 ? deal.images[0] : deal.img,
-              fit: BoxFit.contain,
-              height: 80,
-              width: 80,
+            Row(
+              children: <Widget>[
+                Image.network(
+                  deal.images.length > 0 ? deal.images[0] : deal.img,
+                  fit: BoxFit.contain,
+                  height: 80,
+                  width: 80,
+                ),
+                SizedBox(width: 15),
+                Flexible(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          deal.brand,
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                          maxLines: 1,
+                        ),
+                        Text(deal.name,
+                            style: TextStyle(color: Colors.black87, fontSize: 18),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis),
+                        Row(
+                          children: priceItems,
+                        )
+                      ]
+                  ),
+                ),
+              ],
             ),
-            SizedBox(width: 15),
-            new Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            GestureDetector(
+              onTap: () {
+                showDialog(context: context, builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Container(
+                      height: 152,
+                      child: Stack(
+                        overflow: Overflow.visible,
+                        alignment: Alignment.center,
                         children: <Widget>[
-                          Text(
-                            deal.brand,
-                            style: TextStyle(
-                                color: Colors.black54,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold),
-                            maxLines: 1,
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              showDialog(context: context, builder: (BuildContext context) {
-                                return AlertDialog(
-                                  content: Container(
-                                    height: 152,
-                                    child: Stack(
-                                      overflow: Overflow.visible,
-                                      alignment: Alignment.center,
-                                      children: <Widget>[
-                                        Column(
-                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                          children: <Widget>[
-                                            Row(
-                                              children: <Widget>[
-                                                Icon(
-                                                  Icons.whatshot,
-                                                  color: Theme.of(context).primaryColor,
-                                                ),
-                                                SizedBox(width: 10,),
-                                                Text('Top Brand', style: TextStyle(color: Theme.of(context).primaryColor,),),
-                                              ],
-                                            ),
-                                            Row(
-                                              children: <Widget>[
-                                                Icon(
-                                                  Icons.star,
-                                                  color: Theme.of(context).primaryColor,
-                                                ),
-                                                SizedBox(width: 10,),
-                                                Text('4+ Stars', style: TextStyle(color: Theme.of(context).primaryColor,),),
-                                              ],
-                                            ),
-                                            Row(
-                                              children: <Widget>[
-                                                Icon(
-                                                  Icons.attach_money,
-                                                  color: Theme.of(context).primaryColor,
-                                                ),
-                                                SizedBox(width: 10,),
-                                                Text('Deal of the Day', style: TextStyle(color: Theme.of(context).primaryColor,),),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                        Positioned(
-                                          top: -90,
-                                          child: ClipRRect(
-                                            borderRadius: BorderRadius.circular(10),
-                                            child: Image.asset(
-                                              "assets/icon_120.jpg",
-                                              fit: BoxFit.cover,
-                                              height: 80,
-                                              width: 80,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  contentPadding: EdgeInsets.fromLTRB(40, 50, 40, 0),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      child: Text("OK"),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    )
-                                  ],
-                                );
-                              });
-                            },
-                            child: Row(
-                              children: <Widget>[
-                                if (deal.themes.contains("TopBrand"))
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
                                   Icon(
                                     Icons.whatshot,
-                                    size: 14,
                                     color: Theme.of(context).primaryColor,
                                   ),
-                                if (deal.themes.contains("FourStars"))
+                                  SizedBox(width: 10,),
+                                  Text('Top Brand', style: TextStyle(color: Theme.of(context).primaryColor,),),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
                                   Icon(
                                     Icons.star,
-                                    size: 14,
                                     color: Theme.of(context).primaryColor,
                                   ),
-                                if (deal.themes.contains("DealOfTheDay"))
+                                  SizedBox(width: 10,),
+                                  Text('4+ Stars', style: TextStyle(color: Theme.of(context).primaryColor,),),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
                                   Icon(
                                     Icons.attach_money,
-                                    size: 14,
                                     color: Theme.of(context).primaryColor,
-                                  )
-                              ],
+                                  ),
+                                  SizedBox(width: 10,),
+                                  Text('Deal of the Day', style: TextStyle(color: Theme.of(context).primaryColor,),),
+                                ],
+                              )
+                            ],
+                          ),
+                          Positioned(
+                            top: -90,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(
+                                "assets/icon_120.jpg",
+                                fit: BoxFit.cover,
+                                height: 80,
+                                width: 80,
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      Text(deal.name,
-                          style: TextStyle(color: Colors.black87, fontSize: 18),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis),
+                    ),
+                    contentPadding: EdgeInsets.fromLTRB(40, 50, 40, 0),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
                     ],
+                  );
+                });
+              },
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: 60,
+                  height: 40,
+                  color: Colors.transparent,
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        if (deal.themes.contains("TopBrand"))
+                          Icon(
+                            Icons.whatshot,
+                            size: 14,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        if (deal.themes.contains("FourStars"))
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        if (deal.themes.contains("DealOfTheDay"))
+                          Icon(
+                            Icons.attach_money,
+                            size: 14,
+                            color: Theme.of(context).primaryColor,
+                          )
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: priceItems,
-                  )
-                ],
-              ),
+                )
+              )
             ),
           ],
         ),
