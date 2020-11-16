bf09ce4d097bd30989a034e29a838bdbda34f5b8
claireyang7725 <claireyang7725@gmail.com>
2020-11-15 23:36:01 -0600

    Add bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,55 @@ import 'deals_view/deals_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(DealsCircleApp());
+  runApp(DealCirclesApp());
 }
 
-class DealsCircleApp extends StatelessWidget {
+class DealCirclesApp extends StatefulWidget {
+  @override
+  _DealCirclesAppState createState() => _DealCirclesAppState();
+}
+
+class _DealCirclesAppState extends State<DealCirclesApp> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DealCircles',
-      theme: ThemeData(
-        primaryColor: Color(0xFF632ade),
-        fontFamily: GoogleFonts.openSans().fontFamily
-      ),
-      onGenerateRoute: (settings) {
-        return MaterialPageRoute(
-          builder: (context) => DealsView(id: settings.name.split('/').last)
-        );
-      },
-      debugShowCheckedModeBanner: false
+        title: 'DealCircles',
+        theme: ThemeData(
+            primaryColor: Color(0xFF632ade),
+            fontFamily: GoogleFonts.openSans().fontFamily),
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+            builder: (context) => buildScreen(settings),
+          );
+        },
+        debugShowCheckedModeBanner: false);
+  }
+
+  Widget buildScreen(settings) {
+    return new Scaffold(
+      body: DealsView(id: settings.name.split('/').last),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (int index) => setState(() {
+        _currentIndex = index;
+      }), // new
+      currentIndex: _currentIndex, // new
+      items: [
+        new BottomNavigationBarItem(
+          icon: Icon(Icons.attach_money),
+          label: 'Daily Deals',
+        ),
+        new BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          label: 'Price Alerts',
+        ),
+      ],
     );
   }
 }
