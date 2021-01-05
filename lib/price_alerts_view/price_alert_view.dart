1070d2af22b818bc3604e6156e9591ab7ae96ac1
Yan Zhan <yanzhan91@gmail.com>
2021-01-05 01:31:11 -0600

    Price alert view in progress

--- lib/price_alerts_view/price_alert_view.dart
+++ lib/price_alerts_view/price_alert_view.dart
@@ -1,4 +1,5 @@
-import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'dart:async';
+
 import 'package:dealcircles_flutter/price_alerts_view/price_alert.dart';
 import 'package:dealcircles_flutter/price_alerts_view/price_alert_add_view.dart';
 import 'package:dealcircles_flutter/price_alerts_view/price_alert_alert_dialog.dart';
@@ -17,14 +18,25 @@ class PriceAlertView extends StatefulWidget {
 class _PriceAlertView extends State<PriceAlertView> {
   bool loading = false;
   List<PriceAlert> priceAlerts;
+  ScrollController _scrollController = new ScrollController();
 
   @override
   void initState() {
     priceAlerts = [
-      PriceAlert(PriceAlertType.URL, 'gb Lufta Sleeper Playard, Mink, gb Lufta Sleeper Playard, Mink', '\$15.67', '\$13',
-          'https://images-na.ssl-images-amazon.com/images/I/91w5gn1TEHL._SL1500_.jpg', 'link'),
-      PriceAlert(PriceAlertType.BRAND_OR_STORE, 'Nike', null, null,
-          'https://i.pinimg.com/originals/33/e6/3d/33e63d5adb0da6b303a83901c8e8463a.png', 'null'),
+      PriceAlert(
+          PriceAlertType.URL,
+          'gb Lufta Sleeper Playard, Mink, gb Lufta Sleeper Playard, Mink',
+          '\$15.67',
+          '\$13',
+          'https://images-na.ssl-images-amazon.com/images/I/91w5gn1TEHL._SL1500_.jpg',
+          'link'),
+      PriceAlert(
+          PriceAlertType.BRAND_OR_STORE,
+          'Nike',
+          null,
+          null,
+          'https://i.pinimg.com/originals/33/e6/3d/33e63d5adb0da6b303a83901c8e8463a.png',
+          'null'),
       PriceAlert(PriceAlertType.KEYWORD, 'Ipad', null, null, null, 'null')
     ];
     super.initState();
@@ -33,78 +45,80 @@ class _PriceAlertView extends State<PriceAlertView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.1,
-        centerTitle: false,
-        title: Text(
-          'DealCircles',
-          overflow: TextOverflow.visible,
-          style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-        leading: Image.asset("assets/logo.png"),
-        backgroundColor: Theme.of(context).primaryColor,
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.add_circle_outline,
-              color: Colors.white,
-              size: 28,
+        appBar: AppBar(
+          elevation: 0.1,
+          centerTitle: false,
+          title: Text(
+            'DealCircles',
+            overflow: TextOverflow.visible,
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 28),
+          ),
+          leading: Image.asset("assets/logo.png"),
+          backgroundColor: Theme.of(context).primaryColor,
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.add_circle_outline,
+                color: Colors.white,
+                size: 28,
+              ),
+              onPressed: () => _getNewPriceAlert(context),
             ),
-            onPressed: () => _getNewPriceAlert(context),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 20,),
-          Text(
-            "Create a price alert to be notified\nabout deals for you.",
-            // "Create a price alert\n\nWe will notify you when we find deals of products on your list",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 20,),
-          _generateListview(context),
-        ],
-      )
-
-    );
+          ],
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Create a price alert to be notified\nabout deals for you.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _generateListview(context),
+          ],
+        ));
   }
 
   Future _getNewPriceAlert(BuildContext context) async {
-    final result = await Navigator.push(context,
-        MaterialPageRoute(builder: (context) => PriceAlertAddView()));
+    final result = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => PriceAlertAddView()));
 
     if (result != null) {
       setState(() {
         priceAlerts.add(result);
+        _scrollController.animateTo(_scrollController.position.maxScrollExtent + 80,
+            duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
       });
     }
   }
 
   Widget _generateListview(BuildContext context) {
-    if (priceAlerts.length > 0) {
-      return _appView(context);
-    } else if (loading) {
+    if (loading) {
       return Center(
         child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
+          valueColor:
+              AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
         ),
       );
     } else {
-      return createZeroAlertView();
+      priceAlerts
+          .sort((a, b) => b.type.toString().compareTo(a.type.toString()));
+      return Expanded(
+        child: ListView.builder(
+          controller: _scrollController,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: priceAlerts.length,
+          itemBuilder: (BuildContext context, int index) => createCard(index),
+        ),
+      );
     }
-  }
-
-  Widget _appView(BuildContext context) {
-    priceAlerts.sort((a,b) => b.type.toString().compareTo(a.type.toString()));
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: priceAlerts.length,
-      itemBuilder: (BuildContext context, int index) => createCard(index)
-    );
   }
 
   Widget createCard(index) {
@@ -122,14 +136,16 @@ class _PriceAlertView extends State<PriceAlertView> {
   }
 
   void showCardDetailDialog(index) {
-    showDialog(context: context,builder: (BuildContext context) {
-      if (priceAlerts[index].type == PriceAlertType.URL) {
-        return PriceAlertProductDialog(priceAlerts[index], false,
-            priceAlerts[index].threshold);
-      } else {
-        return PriceAlertAlertDialog(priceAlerts[index]);
-      }
-    }).then((value) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          if (priceAlerts[index].type == PriceAlertType.URL) {
+            return PriceAlertProductDialog(
+                priceAlerts[index], false, priceAlerts[index].threshold);
+          } else {
+            return PriceAlertAlertDialog(priceAlerts[index]);
+          }
+        }).then((value) {
       if (value is bool) {
         if (value == true) {
           setState(() {
@@ -153,9 +169,7 @@ class _PriceAlertView extends State<PriceAlertView> {
       priceItems.add(Text(
         priceAlert.price,
         style: TextStyle(
-          color: Theme
-              .of(context)
-              .primaryColor,
+          color: Theme.of(context).primaryColor,
           fontSize: 14,
           fontWeight: FontWeight.bold,
         ),
@@ -196,8 +210,7 @@ class _PriceAlertView extends State<PriceAlertView> {
                   Text(priceAlert.text,
                       style: TextStyle(color: Colors.black87, fontSize: 18),
                       maxLines: 2,
-                      overflow: TextOverflow.ellipsis
-                  ),
+                      overflow: TextOverflow.ellipsis),
                   if (priceAlerts[index].type == PriceAlertType.URL)
                     Row(
                       children: priceItems,
@@ -207,18 +220,6 @@ class _PriceAlertView extends State<PriceAlertView> {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget createZeroAlertView() {
-    return Center(
-      heightFactor: 3,
-      child: Text(
-        "Create a price alert to be notified about deals you care about.",
-        // "Create a price alert\n\nWe will notify you when we find deals of products on your list",
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 20),
       ),
     );
   }
